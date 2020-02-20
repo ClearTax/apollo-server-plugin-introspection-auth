7ae606af5388c4dee75b3ddf1b0ed16c5d7fac25
Aditi Mohanty <rheaditi@users.noreply.github.com>
2020-02-20 19:54:56 +0530

    Remove condition

--- src/utils.ts
+++ src/utils.ts
@@ -6,9 +6,5 @@ const REGEX_INTROSPECTION_QUERY = /\b(__schema|__type)\b/;
  * Identify if the request is an introspection query type
  * @param request {GraphQLRequestContext['request]}
  */
-export const isIntrospectionRequest = (request: GraphQLRequestContext<any>['request']) => {
-  if (typeof request.query === 'string') {
-    return REGEX_INTROSPECTION_QUERY.test(request.query);
-  }
-  return typeof request.query === 'string' && REGEX_INTROSPECTION_QUERY.test(request.query)
-};
+export const isIntrospectionRequest = (request: GraphQLRequestContext<any>['request']) =>
+  typeof request.query === 'string' && REGEX_INTROSPECTION_QUERY.test(request.query);
